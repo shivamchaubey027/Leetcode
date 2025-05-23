d363d230d5b6eb78da83e39db1b99ddbd049deef
Shivam Sanjay Chaubey <137146026+shivamchaubey027@users.noreply.github.com>
2025-05-24 02:35:02 +0530

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+##Binary search Classic college way, go in middle and see if it is there if not move , accha I wanted to use recursion but IDK how to do that now, so maybe later
