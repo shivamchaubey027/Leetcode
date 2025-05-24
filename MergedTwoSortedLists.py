5070ab3f61eaca1681dac6ca24742d0587959be9
Shivam Sanjay Chaubey <137146026+shivamchaubey027@users.noreply.github.com>
2025-05-24 13:29:13 +0530

    Update MergedTwoSortedLists.py

--- MergedTwoSortedLists.py
+++ MergedTwoSortedLists.py
@@ -26,3 +26,18 @@ class Solution:
 
 
 ##Here we just do the same m+n solution we check the heads of it and go on to merge the listNode behind the ListNode which is smaller, Accha we can do recurssion thing too but we avoid it here
+
+##Okay I did write the recursionb wala solution, It wasnt  very difficult, Do we really need no loops in recursion , Woahh
+##Also I did get stuckinlist1.val which outputs only the value but we had to return the whole list1, object
+ if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if(list1.val< list2.val):
+            list1.next= self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next= self.mergeTwoLists(list1, list2.next)
+            return list2
+
